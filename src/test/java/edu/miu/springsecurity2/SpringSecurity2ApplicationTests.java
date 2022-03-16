9ae1ef8789f230f83e11a9eeb8b1b160eb99e431
umur inan <umurinan@gmail.com>
2022-03-16 07:06:40 -0500

    initial

---
+++ src/test/java/edu/miu/springsecurity2/SpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.springsecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
